df6bd3020404b33b15d0685f41d26b5e06f9ffa3
apatel94 <35053616+apatel94@users.noreply.github.com>
2018-06-06 14:03:49 -0400

    Add files via upload

---
+++ services/mySrv.js
@@ -0,0 +1,31 @@
+﻿(function() {
+    //  var app = angular.module('myApp', []);
+    app.factory('mySrv', ['$timeout', '$q', function($timeout, $q) {
+
+        var service = {};
+
+        // ashynchronous method (2)
+        service.addition = function(a, b) {
+            var obj = $q.defer(); //(3)
+
+            $timeout(function() {
+                var response = "Additoin is :" + (a + b);
+                //var response = "Additoin is :" + (a + b);
+                //obj.resolve(response); // (5) returing response by resolving the promise
+                obj.reject("Hey there is error!!");
+            }, 5000);
+
+            return obj.promise; //(4)
+        }
+        service.substract = function(a, b) {
+            return "Substraction is :" + (a - b);
+        }
+        service.multiply = function(a, b) {
+            return "Multiplication is :" + (a * b);
+        }
+
+        return service;
+
+    }])
+
+})();
---
+++ services/mySrv1.js
@@ -0,0 +1,36 @@
+﻿(function() {
+    //  var app = angular.module('myApp', []);
+    app.factory('mySrv1', ['$timeout', '$q', '$http', function($timeout, $q, $http) {
+
+        var service = {};
+
+        var request = {
+            method: 'get',
+            url: 'status.json',
+            contentType: 'application/json',
+            datatype: 'json'
+        }
+        service.getStudentsInfo = function() {
+            var obj = $q.defer();
+            $timeout(function() {
+                $http(request).then(function success(response) {
+                    // successfull response from status.json
+                    obj.resolve(response.data.students); // data : {students : []}
+                }, function failure(errorMessage) {
+                    obj.reject("Some error");
+                }, function notify(message) {
+                    obj.notify("Please wait processing!!");
+                })
+            }, 10000);
+
+            return obj.promise;
+        }
+
+        // $http.get('app.json').then();
+
+
+
+        return service;
+
+    }])
+})();
